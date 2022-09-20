9a97c5e836226a76996b0f17a745616c91cef99d
Agnieszka Pióro <pioro-agnieszka@wp.pl>
2022-09-20 18:47:12 +0200

    new

--- Kodilla-intro/src/main/java/HelloWorld.java
+++ Kodilla-intro/src/main/java/HelloWorld.java
@@ -1,8 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
+
+        int liczba = 12;
+
+
         System.out.println("Hello World!");
         System.out.println("Aga");
         System.out.println(33);
         System.out.println('P');
+        System.out.println(liczba);
     }
 }
